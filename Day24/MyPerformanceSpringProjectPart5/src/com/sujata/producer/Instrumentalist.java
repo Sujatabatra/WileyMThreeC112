1dab4711fc69c8383c46ef3e6133a3f9eb0d94b1
Sujatabatra <sujatabatra@hotmail.com>
2021-07-01 11:43:08 +0530

    Add files via upload

---
+++ Day24/MyPerformanceSpringProjectPart5/src/com/sujata/producer/Instrumentalist.java
@@ -0,0 +1,17 @@
+package com.sujata.producer;
+
+public class Instrumentalist implements Performer {
+
+	private Instrument instrument;
+
+	public void setInstrument(Instrument instrument) {
+		this.instrument = instrument;
+	}
+
+	@Override
+	public void perform() {
+		System.out.print("Instrumentalist is playing ");
+		instrument.play();
+	}
+
+}
