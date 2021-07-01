2f260bd6556c74c2bd7fefe63099f56c034b4421
Sujatabatra <sujatabatra@hotmail.com>
2021-07-01 13:29:14 +0530

    Add files via upload

---
+++ Day24/MyPerformanceSpringAnnotationProject/src/com/sujata/producer/Singer.java
@@ -0,0 +1,14 @@
+package com.sujata.producer;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Singer implements Performer {
+
+	@Override
+	public void perform() {
+		System.out.println("Singer is singing LALALALA!");
+
+	}
+
+}
---
+++ Day24/MyPerformanceSpringProjectPart6/src/com/sujata/producer/Instrumentalist.java
@@ -0,0 +1,26 @@
+package com.sujata.producer;
+
+public class Instrumentalist implements Performer {
+
+	private Instrument instrument;
+
+	public Instrumentalist() {
+		
+	}
+	
+	public Instrumentalist(Instrument instrument) {
+		super();
+		this.instrument = instrument;
+	}
+
+	public void setInstrument(Instrument instrument) {
+		this.instrument = instrument;
+	}
+
+	@Override
+	public void perform() {
+		System.out.print("Instrumentalist is playing ");
+		instrument.play();
+	}
+
+}
